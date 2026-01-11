e09a0fbbae4a1afb46ae36d0fe8d7aff888c6145
smithwhere <96646318+smithwhere@users.noreply.github.com>
2026-01-11 18:38:24 +0800

    Update jemalloc.sh

--- jemalloc.sh
+++ jemalloc.sh
@@ -3,7 +3,9 @@ wget https://github.com/smithwhere/jemalloc/releases/download/jemalloc/jemalloc-
 bzip2 -d jemalloc-5.3.0.tar.bz2
 tar -xf jemalloc-5.3.0.tar
 cd jemalloc-5.3.0
-./autogen.sh
+#./autogen.sh
+./configure
+#./configure --prefix=/usr/local/jemalloc-5.3.0
 make && make install
 sudo ldconfig
 cd ..
