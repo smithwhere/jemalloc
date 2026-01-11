420b64ba348141b5b0434756037113f884f0d9b9
smithwhere <96646318+smithwhere@users.noreply.github.com>
2026-01-11 16:15:07 +0800

    Update jemalloc.sh

--- jemalloc.sh
+++ jemalloc.sh
@@ -5,5 +5,6 @@ tar -xf jemalloc-5.3.0.tar
 cd jemalloc-5.3.0
 ./autogen.sh
 ./configure --prefix=/usr/local/jemalloc-5.3.0
-make
-make install
+make && make install
+echo '/usr/local/lib' > /etc/ld.so.conf.d/local.conf
+ldconfig
